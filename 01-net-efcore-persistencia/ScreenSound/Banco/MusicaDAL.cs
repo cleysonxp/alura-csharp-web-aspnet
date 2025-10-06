922be7d7a9f32860dc888b3d74b8ce6f72c347e9
cleyson <cleyson.ribeiro.777@gmail.com>
2025-10-05 21:05:21 -0300

    Adicionando a parte de MusicaDAL

---
+++ 01-net-efcore-persistencia/ScreenSound/Banco/MusicaDAL.cs
@@ -0,0 +1,46 @@
+﻿using ScreenSound.Banco;
+using ScreenSound.Modelos;
+
+internal class MusicaDAL : DAL<Musica>
+{
+    private readonly ScreenSoundContext context;
+
+    public MusicaDAL(ScreenSoundContext context)
+    {
+        this.context = context;
+    }
+
+    public override IEnumerable<Musica> Listar()
+    {
+
+        return context.Musicas.ToList();
+
+    }
+
+    public override void Adicionar(Musica musica)
+    {
+        context.Musicas.Add(musica);
+        context.SaveChanges();
+
+    }
+
+    public override void Atualizar(Musica musica)
+    {
+        context.Musicas.Update(musica);
+        context.SaveChanges();
+
+    }
+
+    public override void Deletar(Musica musica)
+    {
+        context.Musicas.Remove(musica);
+        context.SaveChanges();
+
+    }
+
+    public Musica? RecuperarPeloNome(string nome)
+    {
+        return context.Musicas.FirstOrDefault(a => a.Nome.Equals(nome));
+
+    }
+}
