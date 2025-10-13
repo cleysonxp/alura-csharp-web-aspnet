06d36352c15e1c530521cadbd4be1d23a9b7c24c
cleyson <cleyson.ribeiro.777@gmail.com>
2025-10-12 21:02:19 -0300

    Adicionando o modo dark, adição de novas pages e menu ajustado.

--- 03-blazor-aspnetcore-app/ScreenSound.Web/Services/ArtistasAPI.cs
+++ 03-blazor-aspnetcore-app/ScreenSound.Web/Services/ArtistasAPI.cs
@@ -3,10 +3,10 @@ using System.Net.Http.Json;
 
 namespace ScreenSound.Web.Services;
 
-public class ArtistaAPI
+public class ArtistasAPI
 {
     private readonly HttpClient _httpClient;
-    public ArtistaAPI(IHttpClientFactory factory)
+    public ArtistasAPI(IHttpClientFactory factory)
     {
         _httpClient = factory.CreateClient("API");
     }
